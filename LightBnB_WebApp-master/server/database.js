080cfc9c1c6127b65d72820077ec150f8bf40886
Allenzzp <131452abc@gmail.com>
2022-01-06 22:58:45 +0000

    implement reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -83,7 +83,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT r.*, p.*
+  FROM reservations r
+  JOIN properties p
+  ON p.id = property_id
+  WHERE guest_id = $1
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => err.message);
 }
 exports.getAllReservations = getAllReservations;
 
